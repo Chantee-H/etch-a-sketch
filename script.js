192a66aac98814b46ab541972e50d19bc4c2d40f
Chantee <Chantee.hoarau123@gmail.com>
2023-01-09 11:09:57 +0200

    Create an user input

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function makeGrid(){
 }
 
 makeGrid();
+
+const userInput = document.getElementById('number');
+
+function updateGrid(){
+    gridContainer.innerHTML = '';
+    gridContainer.style.gridTemplateColumns = `repeat(${userInput.value}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${userInput.value}, 1fr)`;
+    for(let i = 0; i < (userInput.value * userInput.value); i++){
+        const div = document.createElement('div');
+        gridContainer.appendChild(div).className = 'grid-item';
+        div.addEventListener('mouseover', squareColor);
+    }
+}
