b9f8e34ed8894a46b1af712bf0d4e5535e65b3fc
Chantee <Chantee.hoarau123@gmail.com>
2023-01-09 11:01:01 +0200

    Create a 16x16 grid of squares

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gridContainer = document.getElementById('grid-container');
+
+function makeGrid(){
+    gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
+    for(let i = 0; i < 256; i++){
+        const div = document.createElement('div');
+        gridContainer.appendChild(div).className = 'grid-item';
+    }
+}
+
+makeGrid();
